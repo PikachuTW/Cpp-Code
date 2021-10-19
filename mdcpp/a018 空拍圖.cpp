e0a3134bb60c2fe5276afe4e8dc4fbf71ffce755
Tails <82265690+PikachuTW@users.noreply.github.com>
2021-10-19 15:03:38 +0800

    Create a018 空拍圖.cpp

---
+++ mdcpp/a018 空拍圖.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int a;
+	cin >> a;
+	for (int i = 1; i <= a; i++) {
+		for (int p = 1; p <= i; p++) {
+			cout << i;
+		}
+		cout << endl;
+	}
+	for (int i = a - 1; i >= 0; i--) {
+		for (int p = a - 1; p >= a-i ; p--) {
+			cout << i;
+		}
+		cout << endl;
+	}
+}

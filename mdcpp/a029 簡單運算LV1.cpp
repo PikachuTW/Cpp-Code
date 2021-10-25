20552cdfcf9fc73cc924d2bdbee9155b5337be45
Tails <82265690+PikachuTW@users.noreply.github.com>
2021-10-25 19:44:12 +0800

    Update a029 簡單運算LV1.cpp

--- mdcpp/a029 簡單運算LV1.cpp
+++ mdcpp/a029 簡單運算LV1.cpp
@@ -2,21 +2,22 @@
 using namespace std;
 
 int main() {
-	int a[9];
-	for (int i = 0; i < 9; i++) {
-		cin >> a[i];
-	}
-	for (int i = 8; i > 5; i--) {
-		a[i] += a[i - 3] + a[i - 6];
-	}
-	for (int i = 5; i > 2; i--) {
-		a[i] += a[i - 3];
-	}
-	for (int i = 0; i < 9; i++) {
-		cout << a[i] << " ";
-		if (i % 3 == 2) {
-			cout << endl;
+	int a[3][3];
+	for (int i = 0; i < 3; i++) {
+		for (int p = 0; p < 3; p++) {
+			cin >> a[i][p];
 		}
+	}
+	for (int i = 1; i < 3; i++) {
+		for (int p = 0; p < 3; p++) {
+			a[i][p] += a[i - 1][p];
+		}
+	}
+	for (int i = 0; i < 3; i++) {
+		for (int p = 0; p < 3; p++) {
+			cout << a[i][p] << " ";
+		}
+		cout << endl;
 	}
 	return 0;
 }

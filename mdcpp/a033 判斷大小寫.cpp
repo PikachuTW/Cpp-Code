13333d866336cbc25d82a068083c1999b982dd09
Tails <82265690+PikachuTW@users.noreply.github.com>
2021-11-09 14:41:59 +0800

    Create a033 判斷大小寫.cpp

---
+++ mdcpp/a033 判斷大小寫.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	char a;
+	cin >> a;
+	if (int(a) >= 97) {
+		cout << "lowercase";
+	}
+	else {
+		cout << "uppercase";
+	}
+	return 0;
+}
